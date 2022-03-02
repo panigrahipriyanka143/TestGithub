a7a61de7408707bc2b202dc1ec9b4b99dbe207e2
Priyanka <priyanka@gmail.com>
2022-03-02 17:40:49 +0530

    added class demo1.java

--- DemoProject/src/com/demo/demo1.java
+++ DemoProject/src/com/demo/demo1.java
@@ -1,10 +1,10 @@
 package com.demo;
 
-public class test1 {
+public class demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        System.out.println("Good Evening");
 	}
 
 }
